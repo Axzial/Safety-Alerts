d80b8164c16b1a32ab5974e608be9dd39f4db16d
axzial <iopklmiopklm1A>
2021-02-04 20:58:01 +0100

    :rocket: Init Project

---
+++ src/test/java/fr/axzial/safetyalterts/SafetyAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.axzial.safetyalterts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
