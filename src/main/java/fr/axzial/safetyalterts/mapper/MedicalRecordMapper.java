73dd54bc93554100dc8642e1c3761a8d609a5820
axzial <victorbrown37@protonmail.com>
2021-02-16 17:22:13 +0100

    :pencil2: Mapping

---
+++ src/main/java/fr/axzial/safetyalterts/mapper/MedicalRecordMapper.java
@@ -0,0 +1,20 @@
+package fr.axzial.safetyalterts.mapper;
+
+import fr.axzial.safetyalterts.dto.ChildMedicalRecordDto;
+import fr.axzial.safetyalterts.model.MedicalRecord;
+import fr.axzial.safetyalterts.util.TimeUtils;
+import lombok.experimental.UtilityClass;
+import org.modelmapper.ModelMapper;
+
+@UtilityClass
+public class MedicalRecordMapper {
+
+    ModelMapper modelMapper = new ModelMapper();
+
+    public ChildMedicalRecordDto mapChildMedicalRecordDto(MedicalRecord medicalRecord){
+        ChildMedicalRecordDto childMedicalRecordDto = modelMapper.map(medicalRecord, ChildMedicalRecordDto.class);
+        childMedicalRecordDto.setAge(TimeUtils.getAgeFromBirthday(medicalRecord.getBirthdate()));
+        return childMedicalRecordDto;
+    }
+
+}
