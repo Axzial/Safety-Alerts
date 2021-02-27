51909c2eff18c16721c42016f7c3e1f7cd728854
Axzial <axzial@protonmail.com>
2021-02-27 17:29:48 +0100

    :sparkles: Fix TimeUtils + Add/Substract methods

--- src/main/java/fr/axzial/safetyalterts/util/TimeUtils.java
+++ src/main/java/fr/axzial/safetyalterts/util/TimeUtils.java
@@ -2,17 +2,30 @@ package fr.axzial.safetyalterts.util;
 
 import lombok.experimental.UtilityClass;
 
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
 @UtilityClass
 public class TimeUtils {
 
-    public Integer getAgeFromBirthday(String birthday){
-        String pattern = "dd/mm/yyyy";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
-        LocalDateTime localDateTime = LocalDateTime.from(formatter.parse(birthday));
-        return LocalDateTime.now().compareTo(localDateTime);
+    public Integer getAgeFromBirthday(Timestamp birthday){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(Timestamp.from(Instant.now()).getTime());
+        int nowYear = calendar.get(Calendar.YEAR);
+        calendar.setTimeInMillis(birthday.getTime());
+        int birthYear = calendar.get(Calendar.YEAR);
+        return nowYear - birthYear;
+    }
+
+    public Timestamp addYears(Timestamp ts, int years){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(ts.getTime());
+        calendar.add(Calendar.YEAR, years);
+        return new Timestamp(calendar.getTime().getTime());
     }
 
 }
