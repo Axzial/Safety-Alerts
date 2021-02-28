671007adb1558953995082baa140d9f824ac4c0f
Axzial <axzial@protonmail.com>
2021-03-01 00:39:01 +0100

    :zap: Repository JpaSpecificationExecutor

--- src/main/java/fr/axzial/safetyalterts/repository/FireStationRepository.java
+++ src/main/java/fr/axzial/safetyalterts/repository/FireStationRepository.java
@@ -2,8 +2,9 @@ package fr.axzial.safetyalterts.repository;
 
 import fr.axzial.safetyalterts.model.FireStation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FireStationRepository extends JpaRepository<FireStation, Integer> {
+public interface FireStationRepository extends JpaRepository<FireStation, Integer>, JpaSpecificationExecutor {
 }
--- src/main/java/fr/axzial/safetyalterts/repository/MedicalRecordRepository.java
+++ src/main/java/fr/axzial/safetyalterts/repository/MedicalRecordRepository.java
@@ -2,8 +2,9 @@ package fr.axzial.safetyalterts.repository;
 
 import fr.axzial.safetyalterts.model.MedicalRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Integer> {
+public interface MedicalRecordRepository extends JpaRepository<MedicalRecord, Integer>, JpaSpecificationExecutor {
 }
