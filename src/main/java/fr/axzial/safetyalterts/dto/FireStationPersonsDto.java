a8e5d406dc060f29401220ae1cc4508d7743c4fd
Axzial <axzial@protonmail.com>
2021-02-28 23:59:08 +0100

    :bug: FireStationPersonsDto NoArgsConstructor

--- src/main/java/fr/axzial/safetyalterts/dto/FireStationPersonsDto.java
+++ src/main/java/fr/axzial/safetyalterts/dto/FireStationPersonsDto.java
@@ -4,13 +4,16 @@ import fr.axzial.safetyalterts.model.FireStation;
 import fr.axzial.safetyalterts.model.Person;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class FireStationPersonsDto {
 
     FireStation fireStation;
     List<Person> personList;
+
 }
